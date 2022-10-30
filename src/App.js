0a529c08664dbdc3bd04037dc852542c04f5ab7a
yihanYozikua <newy860318@gmail.com>
2022-10-30 13:54:49 +0900

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+<<<<<<< HEAD
 import { Parallax, useParallax, ParallaxProvider } from 'react-scroll-parallax';
 import AOS from 'aos';
 import { HashRouter as Router, Link, Routes, Route } from 'react-router-dom';
@@ -54,6 +55,27 @@ const App = () => {
         
       </Routes>
     </Router>
+=======
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+>>>>>>> 6408ea0 (Initialize project using Create React App)
   );
 }
 
