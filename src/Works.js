21a331e341402059ff6075733a9ac24e6ba0e0c6
yihanYozikua <newy860318@gmail.com>
2022-11-08 13:03:14 +0900

    add works template

--- src/Works.js
+++ src/Works.js
@@ -9,6 +9,35 @@ import Sidebar from './components/Sidebar'
 import worksCss from './css/works.module.scss'
 import { images } from './images.ts'
 
+
+const Projects = ({name, type, roles, img, imgWidth, imgHeight, descriptionPositonChoice}) => {
+  const [descriptionPosition, setDescriptionPosition] = useState();
+
+  var roleRender = roles.map(item => <div className={worksCss.description_contents_prop}>{item}</div>);
+  return(
+    <div className={worksCss.project_container}>
+      <div className={worksCss.project_img_and_name_container}>
+        <img className={worksCss.project_img_prop} src={img} style={{width: imgWidth, height: imgHeight}} alt={name}></img>
+        <div className={worksCss.project_name}>{ name }</div>
+      </div>
+      <div className={worksCss.description_container}>
+        <div className={worksCss.decription_contents_container}>
+          <div className={worksCss.description_title_prop}>Type.</div>
+          <div className={worksCss.description_contents_container}>
+            <div className={worksCss.description_contents_prop}>{ type }</div>
+          </div>
+        </div>
+        <div className={worksCss.decription_contents_container}>
+          <div className={worksCss.description_title_prop}>Role.</div>
+          <div className={worksCss.description_contents_container}>
+            { roleRender }
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export default function Works(){
 
   return(
--- src/images.ts
+++ src/images.ts
@@ -1,7 +1,17 @@
 import about_avatar from './static/img/about_avatar.png'
 import footer_bg from './static/img/footer_bg.png'
+import untitled_img from './statc/img/works_1.png'
+import Q_dinosaur_img from './static/img/works_2.png'
+import everything_is_gonna_be_alright_img from './static/img/works_3.png'
+import surface_apporel_img from './static/img/works_4.png'
+import organ_voice_img from './static/img/works_5.png'
 
 export const images = [
   about_avatar,
-  footer_bg
+  footer_bg,
+  untitled_img,
+  Q_dinosaur_img,
+  everything_is_gonna_be_alright_img,
+  surface_apporel_img,
+  organ_voice_img
 ]
