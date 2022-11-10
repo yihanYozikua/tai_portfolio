3e6aea011946a83e04066d079d174fd5e300100e
hsiao.yihan <hsiao.yihan@linecorp.com>
2022-11-10 23:40:46 +0900

    add elements to anotheries

--- src/works/Anotheries.js
+++ src/works/Anotheries.js
@@ -24,6 +24,27 @@ export default function Anotheries() {
                         tool="Adobe illustratior" 
                         url="https://store.line.me/stickershop/product/14468519" urlTitle="Website"
                         bannerImg={banner} />
+
+                    <div className={anotheriesCss.paragraph_1_container}>
+                        <div className={anotheriesCss.paragraph_1_title}>思い出からもう一つの旅へ</div>
+                        <div className={anotheriesCss.paragraph_1_description_container}>
+                            <div className={anotheriesCss.paragraph_1_description_prop}>AnotheriesはAnotherとmemoriesの意味と合わせた造語です。一つ一つ旅の思い出を集め、「思い出からもう一つの旅へ」をコンセプトに。</div>
+                            <div className={anotheriesCss.paragraph_1_description_prop}>写真を通して私たちが旅先で見たもの・感じたことを共有し、共感を持ってもらい、人々と繋ぎたいと思います。</div>
+                        </div>
+                    </div>
+
+                    
+
+
+
+                    <div className={anotheriesCss.paragraph_1_container}>
+                        <div className={anotheriesCss.paragraph_1_title}>旅行サイトほど情報満載ではないけど、SNS投稿より伝えたいことがある</div>
+                        <div className={anotheriesCss.paragraph_1_description_container}>
+                            <div className={anotheriesCss.paragraph_1_description_prop}>綺麗な景色、美味しいご飯、友達との楽しいお時間、旅行に求めるもの、人それぞれ。</div>
+                            <div className={anotheriesCss.paragraph_1_description_prop}>大事なのは、気持ちを共有すること。</div>
+                            <div className={anotheriesCss.paragraph_1_description_prop}>なので、旅行サイトみたいにアクセスからモデルコースまで詳しく紹介するつもりはないです。でも、自分のSNSのようなただの感想シェアにもなりたくありません。インターネットで載せていない､実際に訪れた人だけ知っていること、試して良かったと思う遊び方など、旅をより一層楽しめるように、私たちの経験談でユーザーにとって有意義の情報も提供したいと思います。</div>
+                        </div>
+                    </div>
                 </div>
 
             </section>
