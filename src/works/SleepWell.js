c28255871d05118df6a1a94994d4bf042146c11c
hsiao.yihan <hsiao.yihan@linecorp.com>
2022-11-11 19:07:23 +0900

    add pages in works

--- src/works/SleepWell.js
+++ src/works/SleepWell.js
@@ -9,6 +9,12 @@ import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import banner from '../static/img/works/cover_sleep.png'
 import sleepCss from '../css/works_sleep.module.scss'
+import sleep_1 from '../static/img/works/sleep_1.png'
+import sleep_2 from '../static/img/works/sleep_2.png'
+import sleep_3 from '../static/img/works/sleep_3.png'
+import sleep_4 from '../static/img/works/sleep_4.png'
+import sleep_5 from '../static/img/works/sleep_5.png'
+import sleep_6 from '../static/img/works/sleep_6.png'
 import { images } from '../images.ts'
 
 export default function SleepWell(){
@@ -66,15 +72,84 @@ export default function SleepWell(){
             </div>
           </div>
 
+          <div className={sleepCss.app_show_container}>
+            <div className={sleepCss.show_container}>
+              <div className={sleepCss.show_contents_container}>
+                <div className={sleepCss.show_img_container}>
+                  <img className={sleepCss.show_img_prop} src={sleep_1} alt="sleep_app_1"></img>
+                  <img className={sleepCss.show_img_prop} src={sleep_2} alt="sleep_app_2"></img>
+                </div>
+                <div className={sleepCss.show_description_container}>
+                  <div className={sleepCss.show_description_title_prop}>わかりやすいデータ分析</div>
+                  <div className={sleepCss.show_description_contents_container}>
+                    <div className={sleepCss.show_description_contents_prop}>就寝・睡眠時間、浅い眠りと深い眠りの割合を詳しく記録し、図で表現します。</div>
+                    <div className={sleepCss.show_description_contents_prop}>睡眠時間のデータに基づき、睡眠効率を算出し、睡眠の質を可視化します。</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={sleepCss.show_container}>
+              <div className={sleepCss.show_contents_container}>
+                <div className={sleepCss.show_img_container}>
+                  <img className={sleepCss.show_img_prop} src={sleep_3} alt="sleep_app_1"></img>
+                  <img className={sleepCss.show_img_prop} src={sleep_4} alt="sleep_app_2"></img>
+                </div>
+                <div className={sleepCss.show_description_container}>
+                  <div className={sleepCss.show_description_title_prop}>スマートアラーム</div>
+                  <div className={sleepCss.show_description_contents_container}>
+                    <div className={sleepCss.show_description_contents_prop}>音と振動で、睡眠段階を判断し、浅い眠りにアラームを起動させることで、ストレスフリーの起床を実現する。</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={sleepCss.show_container}>
+              <div className={sleepCss.show_contents_container}>
+                <div className={sleepCss.show_img_container}>
+                  <img className={sleepCss.show_img_prop} src={sleep_1} alt="sleep_app_5"></img>
+                  <img className={sleepCss.show_img_prop} src={sleep_2} alt="sleep_app_6"></img>
+                </div>
+                <div className={sleepCss.show_description_container}>
+                  <div className={sleepCss.show_description_title_prop}>朝活任務で早寝早起きの生活に</div>
+                  <div className={sleepCss.show_description_contents_container}>
+                    <div className={sleepCss.show_description_contents_prop}>朝ご飯を作る、ランニングなど、朝活任務を設定し、達成したらポイントが貯まるシステムで、早寝早起きの意欲を高める効果を狙います。</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
 
+          <iframe className={sleepCss.prototype_container} width="375" height="667" src="https://xd.adobe.com/embed/c82ed3f5-d277-4d05-8ee5-67f4a493fe9e-9d7d/" frameborder="0" allowfullscreen></iframe>
+
+          <div className={sleepCss.postscript_container}>
+            <div className={sleepCss.postscript_contents_container}>
+              <div className={sleepCss.postscript_paragraph_container}>
+                <div className={sleepCss.postscript_paragraph_title_prop}>制作の振り返り</div>
+              </div>
+              <div className={sleepCss.postscript_paragraph_container}>
+                <div className={sleepCss.postscript_paragraph_prop}>「SLEEP WELL」は授業の課題で制作した作品ですが、Case study をはじめ、Functional map、UI flow、Flow chatで情報アーキテクチャを整理し、ワイヤーフレームとモックアップを通してデザインに落とし込み、最後はプロトタイプを使ってターゲットにテストを実施しました。</div>
+                <div className={sleepCss.postscript_paragraph_prop}>こうして一連のデザインプロセスをしっかり行い、試行錯誤しながら制作することが私にとってとても貴重な経験であり、サービスデザインの深さと面白さを感じるきっかけになりました。</div>
+                <div className={sleepCss.postscript_paragraph_prop}>健康と睡眠に意識を向けるつもりなのに、課題や趣味、つい他のことを優先して、睡眠の質が二の次になってしまいます。私たちが抱えた悩みをもとに、同年代の共通課題を洗い出し、それをテーマにして、「SLEEP WELL」が生まれた。もしいつかアプリが実際に構築されたら、ぜひ手にとってみてもらえば嬉しいです。</div>
+              </div>
+            </div>
+            <div className={sleepCss.especially_thanks_container}>
+              <div className={sleepCss.especially_thanks_contents_container}>
+                <div className={sleepCss.especially_thanks_title_prop}>Especially thanks</div>
+                <div className={sleepCss.especially_thanks_description_prop}>Group member</div>
+                <div className={sleepCss.especially_thanks_description_prop}>徐晨星</div>
+                <div className={sleepCss.especially_thanks_description_prop}>溫子儀</div>
+              </div>
+            </div>
+          </div>
 
         </div>
 
         
         
-        {/* <iframe width="375" height="667" src="https://xd.adobe.com/embed/c82ed3f5-d277-4d05-8ee5-67f4a493fe9e-9d7d/" frameborder="0" allowfullscreen></iframe> */}
       </section>
     </PageLayout>
   );
 }
+
+
+
 
